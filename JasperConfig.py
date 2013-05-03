1f1c61b2d9a976421e46f40ff47494f268429a93
Simon Smith <spsmith5@wisc.edu>
2013-05-03 15:42:09 -0500

    Removed conflicts

--- JasperConfig.py
+++ JasperConfig.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 # Sets up directories
 MusicDir = "Music\\"
 ModelDir = ""
-=======
 # Sets up directories
-MusicDir = "Music\\"
-ModelDir = ""
->>>>>>> 644730e565e5e37a2e9ad6b6d5ff63135f8fe096
