47bdbe815a116bd69fccbda155437b85b056bed6
Jerry Hui <chiwei.hui@gmail.com>
2013-05-07 23:37:38 -0500

    time stamp

--- TheJasper.py
+++ TheJasper.py
@@ -1,4 +1,5 @@
 from JasperEngine import *
+import time
 
 # The Jasper - A Haunted House for CAVE
 # Modeling: Jerry Chen, Natalie Flunker, Hasti Mirkia
@@ -7,6 +8,7 @@ from JasperEngine import *
 # Created for DS501, Spring 2013
 		
 #-------------------------- MAIN PROGRAM --------------------------------------
+timestamp_start = time.time()
 
 theJasper = HauntedHouseEngine()
 
@@ -14,16 +16,17 @@ houseCoord = [-12.7, 2.25, 0]
 landscapeCoord = [ houseCoord[0]-18.36, houseCoord[1]-30.47, 0]
 theJasper.create_entity("fullHouse", "Models\\final.ive", houseCoord, True, False, "Concave", False, "Static")
 theJasper.create_entity("simpleHouse", "Models\\simpleHouse.ive", houseCoord, False, True, "Concave", False, "Static")
-theJasper.AddObject( HouseObjects.ScalableObj("landscape", "Models\\Landscape\\landscape.ive", landscapeCoord, True, False, "Concave", False, "Static", [0.3048,0.3048,0.3048]))
+theJasper.AddObject( HouseObjects.ScalableObj("landscape", "Models\\Landscape\\landscape.ive", landscapeCoord, True, False, "Concave", False, "Static", [0.3048,0.3048,0.3048]) )
 
 ghostMan = theJasper.AddParanormal(Paranormal.Ghost("ghostMan", "ghostman.ive", [0,0,0], "LOOK"))
 # ghostMan = theJasper.AddParanormal(Paranormal.GhostFlyaway("ghostManFlyaway", "ghostman.ive", [-1,-2,0], "LOOK"))
 
 chair = theJasper.AddObject(HouseObjects.BumpableObj("chair1", "Furniture\\chair001.ive",[1,1,0]))
 chair.SetBumpedSound("ChairKick.wav")
-chair = theJasper.AddObject(HouseObjects.BumpableObj("chair2", "Furniture\\chair001.ive",[1,3,0]))
-chair.SetBumpedSound("ChairKick.wav")
+chair = theJasper.AddObject(HouseObjects.BumpableObj("chair2", "Furniture\\chair001.ive",[1,3,0],"ChairKick.wav"))
 
-theJasper.AddMusic("ligeti-atmospheres.wav")
 theJasper.AddMusic("ligeti-lux.wav")
+theJasper.AddMusic("ligeti-atmospheres.wav")
 # VRScript.Interaction.setJumpPoint(0,VRScript.Math.Matrix().setTranslation(VRScript.Math.Vector(-5,-4,0.52115)))
+
+print("Finished loading The Jasper in {0} seconds.".format(time.time()-timestamp_start))
